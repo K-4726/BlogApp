a9de59abd4812ea9dbfe0807b9ef699fede4023d
Kaustubh_Shahapurkar2002 <kaustubh.b.shahapurkar@gmail.com>
2023-05-30 23:27:45 +0530

    Login page

--- index.js
+++ index.js
@@ -5,9 +5,10 @@ const ejs = require("ejs");
 
 const app=express();
 
-app.set('view engine', 'ejs');
 
 app.use(express.static("public"));
+
+
 app.get("/",function(req,res){
     res.sendFile(__dirname+"/index.html");
 });
