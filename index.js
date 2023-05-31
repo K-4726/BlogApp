34c75c12d40381b9914b9a8fd17d362b1f540e11
Kaustubh_Shahapurkar2002 <kaustubh.b.shahapurkar@gmail.com>
2023-06-01 00:31:59 +0530

    Hashing

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express=require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose=require("mongoose");
-const encrypt=require("mongoose-encryption");
+const md5=require("md5");
 const app=express();
 
 
@@ -18,7 +18,7 @@ const userSchema=new mongoose.Schema({
     username:String,
     password: String
 });
-userSchema.plugin(encrypt,{secret:process.env.SECRET, encryptedFields:["password"]});
+
 const User=new mongoose.model("User",userSchema);
 app.get("/",function(req,res){
     res.render("login");
@@ -32,7 +32,7 @@ app.post("/register",function(req,res){
        const newuser=new User({
         email:req.body.ewmail,
         username:req.body.uname,
-        password:req.body.password
+        password:md5(req.body.password)
        });
        newuser.save();
        res.render("home");
